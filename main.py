20dd50f17413c571231f29e9918ab2127ac6c184
Megazarak <transformer729@yahoo.ca>
2025-02-27 20:42:48 -0500

    triangle ship

--- main.py
+++ main.py
@@ -3,25 +3,37 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init()
-
+    
     '''
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     '''
-    
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    #gameloop starts
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
-
+        
+        screen.fill((0,0,0)) #RGB Black, also "black" works
+        
+        ship.draw(screen)
+        
         pygame.display.flip()
+
+        #limit to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/teletraan-1/workspace/github.com/Megazarak/asteroids-game/venv
+setenv VIRTUAL_ENV /home/teletraan-1/workspace/github.com/Megazarak/asteroids_game/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
